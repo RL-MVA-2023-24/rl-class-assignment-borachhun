9d1fb2a59a01404126433514a2e5b18536ab6888
borachhun <borachhunyou@gmail.com>
2024-02-19 15:33:22 +0100

    First change

--- src/train.py
+++ src/train.py
@@ -1,6 +1,16 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
+
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
@@ -12,10 +22,192 @@ env = TimeLimit(
 # ENJOY!
 class ProjectAgent:
     def act(self, observation, use_random=False):
-        return 0
+        if use_random:
+            return np.random.randint(self.nb_action)
+        else:
+            with torch.no_grad():
+                Q = self.model(torch.Tensor(observation).unsqueeze(0).to(device))
+                return Q.argmax().item()
 
-    def save(self, path):
-        pass
+    def save(self, path='model.pt'):
+        torch.save(self.model.state_dict(), path)
 
     def load(self):
-        pass
+        self.model.load_state_dict(torch.load('model.pt'))
+        # target model ?????
+
+    ################
+
+    def __init__(self):
+        self.state_dim = 6
+        self.nb_action = 4
+
+
+        self.batch_size = 100
+        self.gamma = 0.95
+        self.learning_rate = 0.001
+
+
+        self.epsilon_min = 0.01
+        self.epsilon_max = 1.
+        self.epsilon_stop = 1000
+        self.epsilon_delay = 20
+        self.epsilon_step = (self.epsilon_max - self.epsilon_min) / self.epsilon_stop
+
+
+        self.nb_gradient_steps = 1
+        self.update_target_strategy = 'replace'
+        self.update_target_freq = 20
+        self.update_target_tau = 0.005
+
+
+        self.model = DQN(self.state_dim, self.nb_action).to(device)
+        self.target_model = DQN(self.state_dim, self.nb_action).to(device)
+        # self.target_model.load_state_dict(self.model.state_dict())
+        # self.target_model.eval()        #?????????????????????????????
+
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        self.loss = nn.MSELoss()
+
+
+        self.memory = ReplayBuffer(int(1e5))
+
+        
+    
+
+    
+    def gradient_step(self):
+        if len(self.memory) > self.batch_size:
+            X, A, R, Y, D = self.memory.sample(self.batch_size)
+            QYmax = self.model(Y).max(1)[0].detach()
+            update = torch.addcmul(R, 1-D, QYmax, value=self.gamma)
+            QXA = self.model(X).gather(1, A.to(torch.long).unsqueeze(1))
+            loss = self.loss(QXA, update.unsqueeze(1))
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+
+
+    def train(self):
+        max_episode = 200
+
+        episode_return = []
+        episode = 0
+        episode_cum_reward = 0
+        state, _ = env.reset()
+        epsilon = self.epsilon_max
+        step = 0
+
+        while episode < max_episode:
+            # update epsilon
+            if step > self.epsilon_delay:
+                epsilon = max(self.epsilon_min, epsilon-self.epsilon_step)
+
+            # select epsilon-greedy action
+            if np.random.rand() < epsilon:
+                action = self.act(state, use_random=True)
+            else:
+                action = self.act(state)
+
+            # step
+            next_state, reward, done, trunc, _ = env.step(action)
+            self.memory.append(state, action, reward, next_state, done)
+            episode_cum_reward += reward
+
+            
+
+
+
+
+
+
+
+            
+            # train
+            for _ in range(self.nb_gradient_steps): 
+                self.gradient_step()
+            # update target network if needed
+            if self.update_target_strategy == 'replace':
+                if step % self.update_target_freq == 0: 
+                    self.target_model.load_state_dict(self.model.state_dict())
+            if self.update_target_strategy == 'ema':
+                target_state_dict = self.target_model.state_dict()
+                model_state_dict = self.model.state_dict()
+                tau = self.update_target_tau
+                for key in model_state_dict:
+                    target_state_dict[key] = tau*model_state_dict[key] + (1-tau)*target_state_dict[key]
+                self.target_model.load_state_dict(target_state_dict)
+
+
+
+
+
+
+
+
+            # next transition
+            step += 1
+            if done or trunc:
+                episode += 1
+                print("Episode ", '{:3d}'.format(episode), 
+                      ", epsilon ", '{:6.2f}'.format(epsilon), 
+                      ", batch size ", '{:5d}'.format(len(self.memory)), 
+                      ", episode return ", '{:4.1f}'.format(episode_cum_reward),
+                      sep='')
+                state, _ = env.reset()
+                episode_return.append(episode_cum_reward)
+                episode_cum_reward = 0
+            else:
+                state = next_state
+
+        return episode_return
+
+
+
+
+
+class DQN(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.data = []
+        self.index = 0
+
+    def append(self, s, a, r, s_, d):
+        if len(self.data) < self.capacity:
+            self.data.append(None)
+        self.data[self.index] = (s, a, r, s_, d)
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.data, batch_size)
+        return list(map(lambda x: torch.Tensor(np.array(x)).to(device), list(zip(*batch))))
+    
+    def __len__(self):
+        return len(self.data)
+
+
+
+
+
+agent = ProjectAgent()
+scores = agent.train()
+agent.save()

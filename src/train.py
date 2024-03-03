cc48cd42de8279c630b66298c0ac363688d1e8d5
borachhun <borachhunyou@gmail.com>
2024-03-03 23:51:04 +0100

    update

--- src/train.py
+++ src/train.py
@@ -25,11 +25,9 @@ env = TimeLimit(
 class ProjectAgent:
     def act(self, observation, use_random=False):
         if use_random:
-            return np.random.randint(self.nb_action)
+            return np.random.randint(self.nb_actions)
         else:
-            with torch.no_grad():
-                Q = self.model(torch.Tensor(observation).unsqueeze(0).to(device))
-                return Q.argmax().item()
+            return greedy_action(self.model, observation)
 
     def save(self, path='model.pt'):
         torch.save(self.model.state_dict(), path)
@@ -45,24 +43,54 @@ class ProjectAgent:
 
     def __init__(self):
         self.nb_actions = 4
-        self.gamma = 0.95
-        self.batch_size = 100
-        buffer_size = int(1e5)
+        self.gamma = 0.98
+        self.batch_size = 1024
+        buffer_size = 500000
         self.memory = ReplayBuffer(buffer_size)
         self.epsilon_max = 1.
         self.epsilon_min = 0.01
-        self.epsilon_stop = 1000
-        self.epsilon_delay = 20
+        self.epsilon_stop = 10000
+        self.epsilon_delay = 500
         self.epsilon_step = (self.epsilon_max-self.epsilon_min)/self.epsilon_stop
         self.model = DQN(6,4).to(device)
         self.target_model = deepcopy(self.model).to(device)
-        self.criterion = torch.nn.MSELoss()
+        self.criterion = torch.nn.SmoothL1Loss()
         lr = 0.001
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.nb_gradient_steps = 1
-        self.update_target_strategy = 'replace'
-        self.update_target_freq = 20
-        self.update_target_tau = 0.005
+        self.update_target_strategy = 'ema'
+        self.update_target_freq = 500
+        self.update_target_tau = 0.001
+        self.monitoring_nb_trials = 0
+
+    def MC_eval(self, env, nb_trials):
+        MC_total_reward = []
+        MC_discounted_reward = []
+        for _ in range(nb_trials):
+            x,_ = env.reset()
+            done = False
+            trunc = False
+            total_reward = 0
+            discounted_reward = 0
+            step = 0
+            while not (done or trunc):
+                a = greedy_action(self.model, x)
+                y,r,done,trunc,_ = env.step(a)
+                x = y
+                total_reward += r
+                discounted_reward += self.gamma**step * r
+                step += 1
+            MC_total_reward.append(total_reward)
+            MC_discounted_reward.append(discounted_reward)
+        return np.mean(MC_discounted_reward), np.mean(MC_total_reward)
+    
+    def V_initial_state(self, env, nb_trials):
+        with torch.no_grad():
+            for _ in range(nb_trials):
+                val = []
+                x,_ = env.reset()
+                val.append(self.model(torch.Tensor(x).unsqueeze(0).to(device)).max().item())
+        return np.mean(val)
     
     def gradient_step(self):
         if len(self.memory) > self.batch_size:
@@ -73,12 +101,13 @@ class ProjectAgent:
             loss = self.criterion(QXA, update.unsqueeze(1))
             self.optimizer.zero_grad()
             loss.backward()
-            self.optimizer.step()
+            self.optimizer.step() 
     
-    def train(self):
-        max_episode = 200
-
+    def train(self, max_episode):
         episode_return = []
+        MC_avg_total_reward = []
+        MC_avg_discounted_reward = []
+        V_init_state = []
         episode = 0
         episode_cum_reward = 0
         state, _ = env.reset()
@@ -115,25 +144,42 @@ class ProjectAgent:
             step += 1
             if done or trunc:
                 episode += 1
-                print("Episode ", '{:3d}'.format(episode), 
-                      ", epsilon ", '{:6.2f}'.format(epsilon), 
-                      ", batch size ", '{:5d}'.format(len(self.memory)), 
-                      ", episode return ", '{:4.1f}'.format(episode_cum_reward),
-                      sep='')
+                # Monitoring
+                if self.monitoring_nb_trials>0:
+                    MC_dr, MC_tr = self.MC_eval(env, self.monitoring_nb_trials)
+                    V0 = self.V_initial_state(env, self.monitoring_nb_trials)
+                    MC_avg_total_reward.append(MC_tr)
+                    MC_avg_discounted_reward.append(MC_dr)
+                    V_init_state.append(V0)
+                    episode_return.append(episode_cum_reward)
+                    print("Episode ", '{:2d}'.format(episode), 
+                          ", epsilon ", '{:6.2f}'.format(epsilon), 
+                          ", batch size ", '{:4d}'.format(len(self.memory)), 
+                          ", ep return ", '{:4.1f}'.format(episode_cum_reward), 
+                          ", MC tot ", '{:6.2f}'.format(MC_tr),
+                          ", MC disc ", '{:6.2f}'.format(MC_dr),
+                          ", V0 ", '{:6.2f}'.format(V0),
+                          sep='')
+                else:
+                    episode_return.append(episode_cum_reward)
+                    print("Episode ", '{:2d}'.format(episode), 
+                          ", epsilon ", '{:6.2f}'.format(epsilon), 
+                          ", batch size ", '{:4d}'.format(len(self.memory)), 
+                          ", ep return ", '{:4.1f}'.format(episode_cum_reward), 
+                          sep='')
+
                 state, _ = env.reset()
-                episode_return.append(episode_cum_reward)
                 episode_cum_reward = 0
             else:
                 state = next_state
-        return episode_return
+        return episode_return, MC_avg_discounted_reward, MC_avg_total_reward, V_init_state
 
 
 def greedy_action(network, state):
-    # device = "cuda" if next(network.parameters()).is_cuda else "cpu"
     with torch.no_grad():
         Q = network(torch.Tensor(state).unsqueeze(0).to(device))
         return torch.argmax(Q).item()
-
+    
 
 class ReplayBuffer:
     def __init__(self, capacity):
@@ -157,7 +203,7 @@ class DQN(nn.Module):
     def __init__(self, input_size, output_size):
         super(DQN, self).__init__()
 
-        self.hidden_dim = 128
+        self.hidden_dim = 256
 
         self.fc1 = nn.Linear(input_size, self.hidden_dim)
         self.fc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
@@ -167,20 +213,32 @@ class DQN(nn.Module):
         self.fc6 = nn.Linear(self.hidden_dim, self.hidden_dim)
         self.last_fc = nn.Linear(self.hidden_dim, output_size)
 
-        self.activation = nn.LeakyReLU()
+        self.activation = nn.ReLU()
 
     def forward(self, x):
         x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.activation(self.fc3(x))
-        x = self.activation(self.fc4(x))
-        x = self.activation(self.fc5(x))
-        x = self.activation(self.fc6(x))
+        x = self.activation(self.fc2(x)) + x
+        x = self.activation(self.fc3(x)) + x
+        x = self.activation(self.fc4(x)) + x
+        x = self.activation(self.fc5(x)) + x
+        x = self.activation(self.fc6(x)) + x
         x = self.last_fc(x)
         return x
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     agent = ProjectAgent()
-    agent.train()
+
+    # Pre-fill the replay buffer
+    x, _ = env.reset()
+    for t in range(10000):
+        a = env.action_space.sample()
+        y, r, d, tr, _ = env.step(a)
+        agent.memory.append(x, a, r, y, d)
+        if d:
+            x, _ = env.reset()
+        else:
+            x = y
+    
+    agent.train(300)
     agent.save()
